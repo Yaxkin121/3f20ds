d62aeb35a9e693042d9e8aa8e67ae57a14b68655
Yaxkin Sánchez <yaxkin121@gmail.com>
2020-11-18 21:56:44 -0600

    Trabajos

---
+++ 1p/Sets/RndNames.js
@@ -0,0 +1,3 @@
+var ArrNames = ["Yaxkin", "María", "Betty", "Gadd", "Kevin", "Nuria", "Karla", "Carlos", "Viviana", "Mariana"]
+var SetsNames = new Set(ArrNames)
+console.log(SetsNames)
---
+++ 1p/Sets/TomarTiempos.js
@@ -0,0 +1,12 @@
+var SetsNames = new Set()
+var BNames = "name"
+var times = 100000
+
+console.time('Tiempo tomado');
+while(SetsNames.size < times){
+    var numero = (Math.floor(Math.random() * times)).toString()
+    var nombreCompleto = BNames + numero
+    SetsNames.add(nombreCompleto)
+}
+console.log(SetsNames)
+console.timeEnd("Tiempo tomado");
