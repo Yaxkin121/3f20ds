dfe266417760a5c6c61bd88e16103ca1c07a4bf1
Yaxkin Sánchez <yaxkin121@gmail.com>
2020-11-18 21:15:03 -0600

    Trabajos

--- 1p/Recursión/Fibonacci.js
+++ 1p/Recursión/Fibonacci.js
@@ -1,6 +1,6 @@
-function serie(num) {
+function serief(num) {
     if (num == 1) return 0;
     if (num == 2) return 1;
-    return serie(num - 1) + serie(num - 2);
+    return serief(num - 1) + serief(num - 2);
   }
-  console.log(serie(11));
+  console.log(serief(11));
