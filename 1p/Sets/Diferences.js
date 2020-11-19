2bb7c7d4236216b195c478130349c3d0e87dfb59
Yaxkin Sánchez <yaxkin121@gmail.com>
2020-11-18 21:46:13 -0600

    Trabajos

--- 1p/Sets/Diferences.js
+++ 1p/Sets/Diferences.js
@@ -1,1 +1,17 @@
-//El objeto WeakSet  te deja almacenar y mantener objectos debilmente en una colección a la vez que es iterable.
+//El objeto Set te deja almacenar y mantener objectos debilmente en una colección a la vez que es iterable.
+
+// Podemos crear un Array con elementos repetidos dentro.
+var arr = [10, 9, 9, 3, 4, 4]
+
+// Luego creamos un Set con los elementos del Array.
+var MySet = new Set(arr)
+
+//A la hora de imprimir en consola, solamente nos imprimirá los elementos sin repertirlos.
+console.log(MySet);
+
+//Es posible añadir más elementos con .add() y se cumplirá si no se repite.
+MySet.add(11)
+MySet.add(6)
+console.log(MySet)
+
+//Entonces, debido a que los sets son iterables se puede imaginar que los Weaksets no lo son.
